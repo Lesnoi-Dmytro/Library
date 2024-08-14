d7c120a181d668523e5511cc3eb8e7bdf51cc48b
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2024-08-14 11:03:46 +0300

    Completed OpenAPI documentation

--- src/main/java/org/nerdy/soft/library/controller/BorrowController.java
+++ src/main/java/org/nerdy/soft/library/controller/BorrowController.java
@@ -1,5 +1,8 @@
 package org.nerdy.soft.library.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.nerdy.soft.library.data.Book;
 import org.nerdy.soft.library.data.Member;
@@ -15,11 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/borrow")
 @RequiredArgsConstructor
+@Tag(
+		name = "Borrow",
+		description = "Borrow operations"
+)
 public class BorrowController {
 	private final BookService bookService;
 	private final MemberService memberService;
 	private final BorrowService borrowService;
 
+	@Operation(
+			description = "Borrow a book by member. Throws an exception if member or book is not found. " +
+					"Also throws an exception if member reached borrow limit, there is no free book or member already borrowed this book",
+			summary = "Borrow book",
+			responses = {
+					@ApiResponse(
+							description = "Book successfully borrowed by a member",
+							responseCode = "200"
+					),
+					@ApiResponse(
+							description = "Member can't borrow this book",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@PostMapping
 	public void borrowBook(@RequestBody BorrowRequest request) {
 		Book book = bookService.getBookById(request.bookId());
@@ -28,6 +51,22 @@ public class BorrowController {
 		borrowService.borrowBook(book, member);
 	}
 
+	@Operation(
+			description = "Return a book by member. Throws an exception if member or book is not found. " +
+					"Also throws an exception if member didn't borrowed this book",
+			summary = "Return book",
+			responses = {
+					@ApiResponse(
+							description = "Book successfully returned by a member ",
+							responseCode = "200"
+					),
+					@ApiResponse(
+							description = "Member can't return this book",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@PostMapping("/return")
 	public void returnBook(@RequestBody BorrowRequest request) {
 		Book book = bookService.getBookById(request.bookId());
--- src/main/java/org/nerdy/soft/library/controller/MemberController.java
+++ src/main/java/org/nerdy/soft/library/controller/MemberController.java
@@ -1,5 +1,13 @@
 package org.nerdy.soft.library.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.nerdy.soft.library.data.Book;
 import org.nerdy.soft.library.data.Member;
@@ -12,35 +20,167 @@ import java.util.List;
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
+@Tag(
+		name = "Members",
+		description = "Member CRUD operations"
+)
 public class MemberController {
 	private final MemberService memberService;
 
+	@Operation(
+			description = "All members retrieval. Results are pageable",
+			summary = "All members",
+			parameters = {
+					@Parameter(
+							in = ParameterIn.QUERY,
+							required = true,
+							name = "page",
+							description = "Page number, starts from 0",
+							example = "1"),
+					@Parameter(
+							in = ParameterIn.QUERY,
+							required = true,
+							name = "size",
+							description = "Page size",
+							example = "20")
+			},
+			responses = {
+					@ApiResponse(
+							description = "Page of members",
+							responseCode = "200"
+					)
+			}
+	)
 	@GetMapping
 	public Page<Member> getAllMembers(@RequestParam int page,
-								  @RequestParam int size) {
+									  @RequestParam int size) {
 		return memberService.getMembers(page, size);
 	}
 
+	@Operation(
+			description = "Returns member by id. Throws error if there is no such member",
+			summary = "Member by id",
+			parameters = {
+					@Parameter(
+							in = ParameterIn.PATH,
+							required = true,
+							name = "id",
+							description = "Member id",
+							example = "1")
+			},
+			responses = {
+					@ApiResponse(
+							description = "Member with specified id",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Member.class)
+							)
+					),
+					@ApiResponse(
+							description = "Member with specified id not found",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@GetMapping("/{id}")
 	public Member getMemberById(@PathVariable long id) {
 		return memberService.getMemberById(id);
 	}
 
+	@Operation(
+			description = "Create a member. Membership date assigned automatically",
+			summary = "Create member",
+			responses = {
+					@ApiResponse(
+							description = "Created member",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Member.class)
+							)
+					)
+			}
+	)
 	@PostMapping
 	public Member createMember(@RequestBody Member member) {
 		return memberService.createMember(member);
 	}
 
+	@Operation(
+			description = "Update a member. Throws error if there is no such book",
+			summary = "Update member",
+			responses = {
+					@ApiResponse(
+							description = "Updated member",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Member.class)
+							)
+					),
+					@ApiResponse(
+							description = "Member with specified id not found",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@PutMapping
 	public Member updateBook(@RequestBody Member member) {
 		return memberService.updateMember(member);
 	}
 
+	@Operation(
+			description = "Delete a member. Throws error if there is no such book",
+			summary = "Delete member",
+			responses = {
+					@ApiResponse(
+							description = "Member deleted successfully",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									schema = @Schema(implementation = Member.class)
+							)
+					),
+					@ApiResponse(
+							description = "Member with specified id not found",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@DeleteMapping("/{id}")
 	public void deleteMember(@PathVariable long id) {
 		memberService.deleteMember(id);
 	}
 
+	@Operation(
+			description = "All books borrowed by member with a specified name. Throws error if there is no such member",
+			summary = "Borrowed books by name",
+			parameters = {
+					@Parameter(in = ParameterIn.PATH,
+							required = true,
+							description = "Member name",
+							example = "John Doe")
+			},
+			responses = {
+					@ApiResponse(
+							description = "Updated member",
+							responseCode = "200",
+							content = @Content(
+									mediaType = "application/json",
+									array = @ArraySchema(schema = @Schema(implementation = Book.class))
+							)
+					),
+					@ApiResponse(
+							description = "Member with specified name not found",
+							responseCode = "400",
+							ref = "#/components/responses/400Response"
+					)
+			}
+	)
 	@GetMapping("/{name}/borrowed")
 	public List<Book> borrowedBooks(@PathVariable String name) {
 		return memberService.findBorrowedByName(name);
